2071aa0d7cab1b3046f1628bca354504fb10e766
anasbenmguirida <anasbenmki@gmail.com>
2025-06-20 17:18:57 +0100

    fixing an underfitting issue

--- ML/Regression/polyn-regression.py
+++ ML/Regression/polyn-regression.py
@@ -1,5 +1,7 @@
 # Modeling Electricity Consumption in Relation to Temperature 
 # => estimer que la consomation s'eleve dans les temperature extermemnt hot/cold et modérée en temperature normaux 
+
+
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 import pandas as pd
@@ -10,18 +12,21 @@ salary_data = pd.read_excel("ML/Regression/poly-salary.xlsx")
 X = salary_data[["Years of Experience"]] 
 Y = salary_data["salary"]
 
-polynomial_model = PolynomialFeatures(degree=2)
+polynomial_model = PolynomialFeatures(degree=4)
+
 X_poly = polynomial_model.fit_transform(X)
 
 model = LinearRegression()
 model.fit(X_poly, Y)
 
-experience = np.array([[7]]) 
-experience_poly = polynomial_model.transform(experience)
-predicted_salary = model.predict(experience_poly)
+experience = np.array([[8]]) # creer un tableau 2d 
+experience_poly = polynomial_model.transform(experience) # transormer sous forme matricielle 
+print(experience_poly)
+predicted_salary = model.predict(experience_poly) # l'utiliser dans le model linear pour predicter 
 print(Y)
-print(f"7 years of experience: {predicted_salary[0]:.2f}")
+print(f"8 years of experience: {predicted_salary[0]:.2f}")
 
-polynomial_model.fit(X_poly, Y)
+
+
 
 
